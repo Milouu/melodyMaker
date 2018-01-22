17fdf6d8a1dd9954d6bbef50d8696683c1cd7459
Milouu <milan.roustan@gmail.com>
2018-01-22 20:16:39 +0100

    Updating gulp settings

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,16 @@
 // JS 
 const gulp = require('gulp')
+const browserSync = require('browser-sync')
+
+gulp.task('browserSync', () =>
+{
+  browserSync({
+    server : {
+      baseDir : 'app'
+    }
+  })
+})
+
 // Stylus
 const stylus = require('gulp-stylus')
 
@@ -8,9 +19,15 @@ gulp.task('stylus', () =>
   return gulp.src('app/styles/stylus/**/*.styl')
   .pipe(stylus())
   .pipe(gulp.dest('app/styles/css'))
+  .pipe(browserSync.reload({
+    stream: true
+  }))
 })
 
-gulp.task('watch', () =>
+gulp.task('watch', ['browserSync', 'stylus'], () =>
 {
   gulp.watch('app/styles/stylus/**/*.styl', ['stylus'])
+  gulp.watch('app/scripts/**/*.js', browserSync.reload)
+  gulp.watch('app/*.html', browserSync.reload)
 })
+

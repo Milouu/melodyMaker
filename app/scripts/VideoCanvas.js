2ea66fa3f1a88f302b55b5c28b0225437af6fe88
Milouu <milan.roustan@gmail.com>
2018-01-25 15:04:10 +0100

    Add findColor() method

--- app/scripts/VideoCanvas.js
+++ app/scripts/VideoCanvas.js
@@ -29,6 +29,24 @@ class VideoCanvas {
 	{
 		if(this.$video.paused || this.$video.ended) { return false }
 		this.context.drawImage(this.$video, 0, 0, this.canvas.width, this.canvas.height)
+		this.imageData = this.context.getImageData(0, 0, this.canvas.width, this.canvas.height)
+		this.findColor()
 		setTimeout(() => { this.draw() }, 20)
+	}
+
+	findColor()
+	{
+		let data = this.imageData.data
+		for(let i = 0; i < data.length ; i+=4)
+		{
+			let r = data[i]
+			let g = data[i+1]
+			let b = data[i+2]
+
+			if((r==241)&&(g==202)&&(b==69))
+			{
+				console.log('it works mofo')
+			}
+		}
 	}
 }

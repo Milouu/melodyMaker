a6774ce9cbc3769567b058f36537b1f3ebb8dd1f
Milouu <milan.roustan@gmail.com>
2018-01-25 15:36:10 +0100

    Getting coordinates from color

--- app/scripts/VideoCanvas.js
+++ app/scripts/VideoCanvas.js
@@ -6,6 +6,7 @@ class VideoCanvas {
 		this.$video.controls = true
 		this.$video.muted = true
 		this.$video.autoplay = true
+		this.$video.currentTime = 15
 
 		this.$body = document.querySelector('body')
 		this.canvas = document.createElement('canvas')
@@ -37,15 +38,22 @@ class VideoCanvas {
 	findColor()
 	{
 		let data = this.imageData.data
+		let counter = 0
 		for(let i = 0; i < data.length ; i+=4)
 		{
 			let r = data[i]
 			let g = data[i+1]
 			let b = data[i+2]
+			counter++
 
 			if((r==241)&&(g==202)&&(b==69))
 			{
-				console.log('it works mofo')
+				let x = Math.floor((i%3200)/4)
+				let y = Math.floor(i/3200) 
+				console.log(`Call n°${counter} : x = ${x} y = ${y}`)
+				this.context.fillStyle = 'red'
+				this.context.arc(x, y, 50, 0, Math.PI*2)
+				this.context.fill()
 			}
 		}
 	}

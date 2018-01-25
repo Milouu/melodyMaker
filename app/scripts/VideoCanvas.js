63faee5701331a014780da856ade0e9bc913adac
vtimsit <victor.timsit@hetic.net>
2018-01-25 18:27:25 +0100

    Fix undefind bug line 39 in VideoCanvas.js

--- app/scripts/VideoCanvas.js
+++ app/scripts/VideoCanvas.js
@@ -20,48 +20,43 @@ class VideoCanvas {
 
 		this.$video.style.display = 'none'
 
-		this.$video.addEventListener('play', () =>
-		{
+		this.$video.addEventListener('play', () => {
 			this.draw()
 		}, false)
 	}
-  
-	draw()
-	{
-		this.clearCanvas()
-		if(this.$video.paused || this.$video.ended) { return false }
+
+	draw() {
 		this.context.drawImage(this.$video, 0, 0, this.canvas.width, this.canvas.height)
 		this.imageData = this.context.getImageData(0, 0, this.canvas.width, this.canvas.height)
 		this.findColor()
-		setTimeout(() => { this.draw() }, 20)
+		setTimeout(() => { 
+			this.clearCanvas()
+			this.draw() 
+		}, 20)
 	}
 
-	findColor()
-	{
-		const data = this.imageData.data
+	findColor() {
+		const data = this.imageData.data		
 		// let counter = 0
-		for(let i = 0; i < data.length ; i+=4)
-		{
+		for (let i = 0; i < data.length; i += 4) {
 			const r = data[i]
-			const g = data[i+1]
-			const b = data[i+2]
+			const g = data[i + 1]
+			const b = data[i + 2]
 			// counter++
-			if((r>=160)&&(g>=160)&&(b<=130))
-			{
-				let x = Math.floor((i%3200)/4)
-				let y = Math.floor(i/3200) 
+			if ((r >= 160) && (g >= 160) && (b <= 130)) {
+				let x = Math.floor((i % 3200) / 4)
+				let y = Math.floor(i / 3200)
 				// console.log(`Call n°${counter} : x = ${x} y = ${y}`)
 				this.context.beginPath() // C'EST CETTE PUTAIN DE LIGNE DE MERDE QUI FAISAIT TOUT BUGUER BORDEL DE CUL
 				this.context.fillStyle = 'red'
-				this.context.arc(x, y, 1, 0, Math.PI*2)
+				this.context.arc(x, y, 1, 0, Math.PI * 2)
 				this.context.fill()
 				this.context.closePath()
 			}
 		}
 	}
 
-	clearCanvas()
-	{
+	clearCanvas() {
 		this.context.clearRect(0, 0, this.canvas.width, this.canvas.height)
 	}
 }

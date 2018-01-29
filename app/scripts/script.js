f5075494a2572f69b2f2463818fad0df7a713f1f
Milouu <milan.roustan@gmail.com>
2018-01-29 18:12:34 +0100

    :fire: Removing useless code

--- app/scripts/script.js
+++ app/scripts/script.js
@@ -1,35 +1,5 @@
-// const notes = document.querySelectorAll('.note')
-
-// class Melody {
-// 	constructor(notes){
-
-// 		this.notes = notes
-// 	}
-
-// 	randomNote() {
-
-// 		this.randomAccess = Math.floor(Math.random() * this.notes.length)
-// 		return this.notes[this.randomAccess]
-// 	}
-// 	play() {
-
-// 		setInterval(() => {
-
-// 			this.randomNote().play()
-
-// 		}, 2000)
-// 	}
-// }
-
-// const test = new Melody(notes)
-// test.play()
-
 const canvasVideo = new CanvasVideo(window.innerWidth, window.innerHeight)
 canvasVideo.draw()
 
 const trackingColor = new TrackingColor(160, 160, 130, canvasVideo)
 trackingColor.findColor()
-
-// const trackingColorTwo = new TrackingColor(103, 55, 161, testVideo)
-// // trackingColorTwo.draw()
-// trackingColorTwo.findColor()

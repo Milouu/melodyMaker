f0d38934e88fe0393e6434726ba978c203570bab
Milouu <milan.roustan@gmail.com>
2018-04-25 17:13:49 +0200

    :fire: Remove useless comments

--- app/scripts/script.js
+++ app/scripts/script.js
@@ -1,14 +1,1 @@
-// const webcam = new Webcam()
-
-// const canvasVideo = new CanvasVideo(window.innerWidth, window.innerHeight, webcam)
-// canvasVideo.draw()
-
-// const eyeDropper = new EyeDropper(canvasVideo)
-
-// const trackingColor = new TrackingColor(canvasVideo, eyeDropper)  
-
-// const drumKit = new DrumKit(trackingColor)
-
-// const oscillator = new Oscillator(trackingColor)
-
-const music = new MusicalCanvas()
+const musicalCanvas = new MusicalCanvas()

832324576700672a71c1db35ae229ab4675fa5c2
vtimsit <victor.timsit@hetic.net>
2018-05-05 18:35:05 +0200

    :sparkles: Loader & lazy loading

--- app/scripts/script.js
+++ app/scripts/script.js
@@ -12,7 +12,7 @@
 // const oscillator = new Oscillator(trackingColor)
 
 const canvas = new WavesCanvas('content__jelly', 'img')
-const trackAnimate = new DOMLoader(['track', 'loop', 'drum'], ['track--animate', 'loop--animate', 'drum--animate'])
+const trackAnimate = new DOMLoader(['container', 'track', 'loop', 'drum', 'loader'], ['container--animate', 'track--animate', 'loop--animate', 'drum--animate', 'loader--unDisplay'])
 
 // const musicalCanvas = new MusicalCanvas()
 

d2318e48d0e72a5fddd13701b34d3a4f7b2e264e
vtimsit <victor.timsit@hetic.net>
2018-05-07 13:19:25 +0200

    :sparkles: Lazy loading

--- app/scripts/DOMLoader.js
+++ app/scripts/DOMLoader.js
@@ -1,15 +1,41 @@
 class DOMLoader
 {   
-    constructor(arrayOfElementsClassName, arrayOfStateClassName) 
+    constructor(sourceClass, arrayOfElementsClassName, arrayOfStateClassName) 
     {
         this.elements = arrayOfElementsClassName
         this.states = arrayOfStateClassName
+        this.sources = document.querySelectorAll('.' + sourceClass)
+
+        this.loader()
+    }
+    loader()
+    {
+        let loaded = -1
 
         window.addEventListener('DOMContentLoaded', () => {
-            for(let i = 0; i < this.elements.length; i++) {
-                const element = document.querySelector('.' + this.elements[i])
-                element.classList.add(this.states[i])
-            }
+            loaded ++
+            loaded == this.sources.length ? this.init() : false
         })
+
+        for(const source of this.sources) 
+        {
+            console.log("oui")
+            const $newImg = document.createElement('img')
+
+            $newImg.addEventListener('load', () => 
+            {
+                loaded++
+                console.log(source)
+                loaded == this.sources.length ? this.init() : false
+            })
+            $newImg.src = source.src
+        }
+    }
+    init()
+    {
+        for(let i = 0; i < this.elements.length; i++) {
+            const element = document.querySelector('.' + this.elements[i])
+            element.classList.add(this.states[i])
+        }
     }
 }
--- app/scripts/script.js
+++ app/scripts/script.js
@@ -12,7 +12,8 @@
 // const oscillator = new Oscillator(trackingColor)
 
 const canvas = new WavesCanvas('content__jelly', 'img')
-const trackAnimate = new DOMLoader(['container', 'track', 'loop', 'drum', 'loader'], ['container--animate', 'track--animate', 'loop--animate', 'drum--animate', 'loader--unDisplay'])
+
+const trackAnimate = new DOMLoader('lazyload', ['container', 'track', 'loop', 'drum', 'loader'], ['container--animate', 'track--animate', 'loop--animate', 'drum--animate', 'loader--unDisplay'])
 
 // const musicalCanvas = new MusicalCanvas()
 

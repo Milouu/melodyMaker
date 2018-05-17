f08cda9dbf29be1f75f4c73838bf8848d890ce7d
Milouu <milan.roustan@gmail.com>
2018-05-17 16:19:49 +0200

    :art: Cleaned master a bit

--- app/scripts/script.js
+++ app/scripts/script.js
@@ -1,17 +1,6 @@
-// const webcam = new Webcam()
-
-// const canvasVideo = new CanvasVideo(window.innerWidth, window.innerHeight, webcam)
-// canvasVideo.draw()
-
-// const eyeDropper = new EyeDropper(canvasVideo)
-
-// const trackingColor = new TrackingColor(canvasVideo, eyeDropper)  
-
-// const drumKit = new DrumKit(trackingColor)
-
 // const oscillator = new Oscillator(trackingColor)
 
-const canvas = new WavesCanvas('waves', 'img')
+// const canvas = new WavesCanvas('waves', 'img')
 
 const musicalCanvas = new MusicalCanvas()
 

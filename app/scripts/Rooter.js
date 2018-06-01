5e759dfa835aebd797e3809b1faf880d92b2f358
vtimsit <victor.timsit@hetic.net>
2018-06-01 18:51:25 +0200

    :sparkles: Rooter

--- app/scripts/Rooter.js
+++ app/scripts/Rooter.js
@@ -5,12 +5,27 @@ class Rooter
         this.$callToAction = document.querySelector('.callToAction')
 
         // Homepage
-        this.$callToAction.addEventListener('click', () => 
+        this.$callToAction.addEventListener('mousedown', () => 
         {
             if(this.$callToAction.classList.contains('callToAction--home'))
             {
+                let mouseUp = false
+
+                // Pause video canvas
                 canvas.stop()
-                new ViewsTransition('calibration', 'domView', 'domView', ['transitionOut', 'track--animate', 'loop--animate', 'drum--animate', 'header__title--animate'], ['transitionTitle--display', 'transitionIn', 'calibrationIn', 'calibrationIn'], ['Calibration', 'MusicalCanvas'])
+
+                this.$callToAction.addEventListener('mouseup', () => { mouseUp = true })
+                setTimeout(() => 
+                {
+                    !mouseUp ? new ViewsTransition
+                    (
+                        'calibration', 'domView', 'domView', 
+                        ['transitionOut', 'track--animate', 'loop--animate', 'drum--animate', 'header__title--animate'], 
+                        ['transitionTitle--display', 'transitionIn', 'calibrationIn', 'calibrationIn'], 
+                        ['Calibration', 'MusicalCanvas']
+                    ) : false
+                
+                }, 1000)
             }
             if(this.$callToAction.classList.contains('callToAction--calibration'))
             {
--- app/scripts/ViewsTransition.js
+++ app/scripts/ViewsTransition.js
@@ -3,52 +3,135 @@ class ViewsTransition
     constructor(newPageName, oldDOMView, newDOMView, transitionOutClass, transitionInClass, classInstances = null)
     {
         this.oldDOMView = document.querySelectorAll('.' + oldDOMView)
+        this.newDOMView = newDOMView
+        this.transitions = transitionInClass
         this.$body = document.querySelector('body')
         this.classes = classInstances
         console.log(this.classes)
 
+        
         setTimeout(() => 
         {
             this.oldDOMView[0].classList.add(transitionOutClass[0])
-        }, 300)
 
+        }, 300)
+        
         for(let i = 1; i < this.oldDOMView.length; i++) 
         {
             this.oldDOMView[i].classList.remove(transitionOutClass[i])
         }
-
+        
+        // document.body.addEventListener('DOMSubtreeModified', () => 
+        // { 
+            //     console.log('MODIFI')
+            //     document.body.addEventListener('DOMContentLoaded', () => 
+            //     {
+                //         console.log('LOADEEEEEEE')
+                //     })
+                // })
         setTimeout(() => 
         { 
             this.oldDOMView[0].remove() 
 
             let views = this.getPage(`views/${newPageName}.html`, 'body', 'body')
+            // this.newViews = []
 
             if(views != undefined)
             {
-                views = document.querySelectorAll('.' + newDOMView)
-                console.log('VIEWS NEW : ' + views)
-                for(const view of views) { view.classList.add('preTransition') }
-                setTimeout(() => 
-                { 
-                    for(let i = 0; i < views.length; i++) 
-                    {
-                        for(const view of views) { view.classList.remove('preTransition') }
-                        views[i].classList.add(transitionInClass[i]) 
-                    }
+                console.log(newDOMView)
+                // do
+                // {
+                //     this.newViews = document.querySelectorAll('.' + newDOMView)
+                //     console.log(this.newViews)
+                // } while(this.newViews.length == 0)
 
-                    for(const classInstance of this.classes) 
-                    {
-                        console.log(classInstance)
-                        let drumKit = ''
-                        let calibration = ''
+                // this.newViews = document.querySelectorAll('.' + newDOMView)
 
-                        // classInstance == 'MusicalCanvas' ? drumKit = new DrumKit() : false
-                        classInstance == 'Calibration' ? calibration = new Calibration() : false
-                        new Rooter()
-                    }
-                }, 10)
+                // Try to catch new dom elements
+                console.log(transitionInClass)
+                const newViews = this.tryCatchDOM(newDOMView, transitionInClass)
+
+                console.log(newViews)
+                
+                // console.log('VIEWS NEW : ' + views)
+                // for(const view of views) { view.classList.add('preTransition') }
+                // setTimeout(() => 
+                // { 
+                //     for(let i = 0; i < views.length; i++) 
+                //     {
+                //         for(const view of views) { view.classList.remove('preTransition') }
+                //         views[i].classList.add(transitionInClass[i]) 
+                //     }
+
+                //     for(const classInstance of this.classes) 
+                //     {
+                //         console.log(classInstance)
+                //         let drumKit = ''
+                //         let calibration = ''
+
+                //         // classInstance == 'MusicalCanvas' ? drumKit = new DrumKit() : false
+                //         classInstance == 'Calibration' ? calibration = new Calibration() : false
+                //         new Rooter()
+                //     }
+                // }, 10)
             }
         }, 1000)
+    }
+
+    tryCatchDOM(viewsClass)
+    {
+        const newViews = document.querySelectorAll('.' + viewsClass)
+
+        if(newViews.length == 0)
+        {
+            console.log('TESTOS')
+            setTimeout(() => 
+            {
+                this.tryCatchDOM(viewsClass)
+            }, 50)         
+        }
+        else
+        {
+            this.toggleClass(newViews)
+            return newViews
+        } 
+    }
+
+    toggleClass(newViews)
+    {
+        const views = newViews
+
+        if(views != undefined)
+        {
+            // for(const view of views) { view.classList.add('preTransition') } 
+
+            setTimeout(() => 
+            { 
+                for(let i = 0; i < views.length; i++) 
+                {
+                    // console.log(transitionInClass)
+                    // console.log(i)
+                    // views[i].classList.remove('preTransition')
+                    console.log(this.transitions[i])
+                    views[i].classList.add(this.transitions[i])
+                    // views[i].classList.add('coucou') 
+                }
+                // views[1].classList.add(this.transitions[1]) 
+                for(const classInstance of this.classes) 
+                {
+                    console.log(classInstance)
+                    let drumKit = ''
+                    let calibration = ''
+                    // classInstance == 'MusicalCanvas' ? drumKit = new DrumKit() : false
+                    classInstance == 'Calibration' ? calibration = new Calibration() : false
+                    new Rooter()
+                }
+            }, 10)
+        }
+        else
+        {
+            this.toggleClass(views, transitionInClass)
+        }
     }
     getPage(url, from, to)
     {

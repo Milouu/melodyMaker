00d7522fa0a93fdf87aa163a8be5736040a59cbf
vtimsit <victor.timsit@hetic.net>
2018-06-09 20:11:07 +0200

    :construction: Dashboard

--- app/scripts/Rooter.js
+++ app/scripts/Rooter.js
@@ -9,35 +9,6 @@ class Rooter
         this.viewsController.initNewDOM(this.currentPageName)
 
         this.tryCatchNewViewButton()
-
-        // // Listening click event on next button
-        // nextButton.addEventListener('mousedown', () => 
-        // {
-        //     // Recovers page name with data-name
-        //     const pageName = nextButton.dataset.name
-        //     console.log(pageName)
-        //     if(nextButton.classList.contains('newViewButton--delay'))
-        //     {
-        //         console.log('DELAY')
-        //         let mouseUp = false
-
-        //         nextButton.addEventListener('mouseup', () => 
-        //         {
-        //             mouseUp = true
-        //         })
-
-        //         setTimeout(() => 
-        //         {
-        //             if(!mouseUp) { new ViewsController(pageName) }
-        //         }, 1000)
-        //     }
-        //     else
-        //     {
-        //         // Instance of ViewsTransition
-        //         new ViewsController(pageName) // essayer de ne pas re instancier viewscotroller car reset this.page
-        //     }
-
-        // })
     }
     tryCatchNewViewButton()
     {

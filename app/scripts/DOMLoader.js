78ee8fc1bccace12c45a1a2cd813745aebf25bad
Milouu <milan.roustan@gmail.com>
2018-06-09 19:28:57 +0200

    :memo: Editing read.me

--- app/scripts/DOMLoader.js
+++ app/scripts/DOMLoader.js
@@ -1,43 +1,43 @@
 class DOMLoader
 {   
-    constructor(sourceClass, loaderClass, loaderUndisplayClass) 
-    {
-        this.loader = document.querySelector('.' + loaderClass)
-        this.loaderUndisplay = loaderUndisplayClass
-        this.sources = document.querySelectorAll('.' + sourceClass)
+	constructor(sourceClass, loaderClass, loaderUndisplayClass) 
+	{
+		this.loader = document.querySelector('.' + loaderClass)
+		this.loaderUndisplay = loaderUndisplayClass
+		this.sources = document.querySelectorAll('.' + sourceClass)
 
-        this.load()
-    }
-    load()
-    {
-        let loaded = -1
+		this.load()
+	}
+	load()
+	{
+		let loaded = -1
 
-        window.addEventListener('DOMContentLoaded', () => {
-            loaded ++
-            loaded == this.sources.length ? this.init() : false
-        })
+		window.addEventListener('DOMContentLoaded', () => {
+			loaded ++
+			loaded == this.sources.length ? this.init() : false
+		})
 
-        for(const source of this.sources) 
-        {
-            const $newImg = document.createElement('img')
+		for(const source of this.sources) 
+		{
+			const $newImg = document.createElement('img')
 
-            $newImg.addEventListener('load', () => 
-            {
-                loaded++
-                loaded == this.sources.length ? this.init() : false
-            })
-            $newImg.src = source.src
-        }
-    }
-    init()
-    {
-        // for(let i = 0; i < this.elements.length; i++) {
-        //     console.log(this.elements[i])
-        //     const element = document.querySelector('.' + this.elements[i])
-        //     element.classList.add(this.states[i])
-        // }
-        this.loader.classList.add(this.loaderUndisplay)
+			$newImg.addEventListener('load', () => 
+			{
+				loaded++
+				loaded == this.sources.length ? this.init() : false
+			})
+			$newImg.src = source.src
+		}
+	}
+	init()
+	{
+		// for(let i = 0; i < this.elements.length; i++) {
+		//     console.log(this.elements[i])
+		//     const element = document.querySelector('.' + this.elements[i])
+		//     element.classList.add(this.states[i])
+		// }
+		this.loader.classList.add(this.loaderUndisplay)
 
-        new Rooter()
-    }
+		new Rooter()
+	}
 }

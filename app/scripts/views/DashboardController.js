f8733caf38fa77684f370e561465ef28bdd99587
vtimsit <victor.timsit@hetic.net>
2018-06-10 18:41:09 +0200

    :lipstick: Dashboard

--- app/scripts/views/DashboardController.js
+++ app/scripts/views/DashboardController.js
@@ -2,33 +2,56 @@ class DashboardController
 {
     constructor()
     {   
-        this.box1 = document.querySelector('.box-1')
         this.end = false 
+        // this.addi = document.querySelector('.dashboard__add')
     }
     add()
     {
+        const addButton = document.querySelector('.dashboard__add')
+        const instruments = document.querySelector('.instruments');
         const timeline = new TimelineMax({onComplete: this.instances, onCompleteScope: this})
 
         timeline 
-            .from(".dashboard", 0.5, {scale: 0, ease: Power3.easeOut})
-            .from(".dashboard__add", 0.2, {scaleX: 0})
-            .from(".dashboard__addPlus", 0.2, {scale: 0}, '-=0.1')
-            .from(".dashboard__reset", 0.2, {scale: 0})
-            .from(".dashboard__seperator", 0.1, {opacity: 0})
-            .from(".dashboard__play", 0.2, {scale: 0})
-            .from(".dashboard__trackInstrument", 0.3, {y: -100, opacity: 0}, '-=0')
-            .from(".dashboard__track", 0.3, {scaleX: 0, ease: Power1.easeOut})
-            .from(".dashboard__userPictureImg", 0.2, {scaleX: 2}, '-=0.1')
-            .from(".dashboard__userPicture", 0.2, {x: -100, scaleX: 0}, '-=0.2')
-            .from(".dashboard__cursor", 0.3, {x: 600, opacity: 0})
-            .staggerFrom(".dashboard__metric", 0.2, {scaleY: 0}, 0.1)
+            .from('.dashboard', 0.5, {scale: 0, ease: Power3.easeOut})
+            .from('.dashboard__add', 0.2, {scaleX: 0})
+            .from('.dashboard__addPlus', 0.2, {scale: 0}, '-=0.1')
+            .from('.dashboard__reset', 0.2, {scale: 0})
+            .from('.dashboard__seperator', 0.1, {opacity: 0})
+            .from('.dashboard__play', 0.2, {scale: 0})
+            .from('.dashboard__trackInstrument', 0.3, {y: -100, opacity: 0}, '-=0')
+            .from('.dashboard__track', 0.3, {scaleX: 0, ease: Power1.easeOut})
+            .from('.dashboard__userPictureImg', 0.2, {scaleX: 2}, '-=0.1')
+            .from('.dashboard__userPicture', 0.2, {x: -100, scaleX: 0}, '-=0.2')
+            .from('.dashboard__cursor', 0.3, {x: 600, opacity: 0})
+            .staggerFrom('.dashboard__metric', 0.2, {scaleY: 0}, 0.1)
+
+        window.addEventListener('click', (event) => {
+            if (addButton.contains(event.target)) {
+                // Clicked in buttonToDisplay
+                TweenMax.to('.dashboard', 0.3, {scale: 1.2, x: '30%', rotationY:'-45', opacity: 0.8, transformOrigin:'center'})
+                TweenMax.to('.instruments', 0.3, {x: '0%', scale: 1, opacity: 1, transformOrigin:'center'})
+            } 
+            else if(instruments.contains(event.target))
+            {
+                console.log('instrument')
+            }
+            else {
+                // Clicked outside buttonToDisplay and window
+                TweenMax.to('.dashboard', 0.3, {scale: 1, x: '0%', rotationY:'0', opacity: 1, transformOrigin:'center'})
+                TweenMax.to('.instruments', 0.3, {x: '0%', scale: 0.6, opacity: 0, transformOrigin:'center'})
+            }
+        })
+        // window.addEventListener('click', () => 
+        // {
+        //     TweenMax.to('.dashboard', 0.3, {scale: 1, x: '0%', transformOrigin:'center'})
+        // })
     }
     remove()
     {
         // const timeline = new TimelineMax({onComplete: this.ending, onCompleteScope: this })
         
         // timeline
-        // .to(".content", 2, {opacity: 0, scale: 4, x: 600, ease:Elastic.easeOut}, 0.1)
+        // .to('.content', 2, {opacity: 0, scale: 4, x: 600, ease:Elastic.easeOut}, 0.1)
     }
     instances()
     {
